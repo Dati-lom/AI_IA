8e0c9545be4e6884f55cef202475aa6baa1871e8
dati-lom <davit13.lomauridze@gmail.com>
2024-09-03 01:32:05 +0400

    [add]added translations + goes to missed question

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import React, { Fragment, useState, useContext } from "react";
+import React, { Fragment, useState, useContext, useEffect } from "react";
 import emailjs from 'emailjs-com'; // Import EmailJS SDK
 import { renderHTML, scrollToElem, handleChosenAnswer } from "./utilities";
 import { Store, useAppContext } from "./AppContext";
 import "./styles.css";
-import data from "./data.json";
+import dataEN from "./dataEN.json";
+import dataGE from "./dataGE.json";
+
+// Translation object
+const translations = {
+  EN: {
+    never: "Never applies to me",
+    sometimes: "Sometimes applies to me",
+    often: "Often applies to me",
+    next: "Next",
+    prev: "Prev",
+    submit: "Submit",
+    assessmentTitle: "Learning Style Assessment",
+    assessmentSubtitle: "How do these statements apply to you?",
+    wellDone: "Well done!",
+    sectionScores: "Scores for each section:",
+    sectionOne: "Section One (Visual)",
+    sectionTwo: "Section Two (Auditory)",
+    sectionThree: "Section Three (Kinesthetic)",
+    emailSuccess: "Email sent successfully!",
+    incompleteMessage: "Oops, looks like you haven't answered all the questions",
+    scrollMessage: "Scroll up to see which questions you've missed out on.",
+    finish: "Finish"
+  },
+  GE: {
+    never: "არ ვრცელდება ჩემზე",
+    sometimes: "ხანდახან ვრცელდება ჩემზე",
+    often: "ხშირად ვრცელდება ჩემზე",
+    next: "შემდეგი",
+    prev: "წინა",
+    submit: "გაგზავნა",
+    assessmentTitle: "სწავლის სტილის განსაზღვრა",
+    assessmentSubtitle: "გაიგე რომელი გერგება ყველაზე მეტად",
+    wellDone: "გილოცავ!",
+    sectionScores: "სექციების ქულები:",
+    sectionOne: "სექცია ერთი (ვიზუალური)",
+    sectionTwo: "სექცია ორი (სმენითი)",
+    sectionThree: "სექცია სამი (კინესთეტიკური)",
+    emailSuccess: "ელფოსტა წარმატებით გაიგზავნა!",
+    incompleteMessage: "უი, როგორც ჩანს, ყველა კითხვას არ უპასუხეთ",
+    scrollMessage: "დაამატეთ ზემოთ, რომ ნახოთ რომელი კითხვები დაგავიწყდათ.",
+    finish: "დასრულება"
+  }
+};
 
 const answerScores = {
   0: 1, // Never applies to me
   1: 2, // Sometimes applies to me
   2: 3  // Often applies to me
 };
-
-const totalQuestions = data.results.length;
+const totalQuestions = 30;
 
 export default function App() {
   const [chosenAnswers, setChosenAnswers] = useState([]);
   const [emailSent, setEmailSent] = useState(false);
+  const [language, setLanguage] = useState('GE'); // State for selected language
+  const [data, setData] = useState(dataGE);
+
+  useEffect(() => {
+    setData(language === 'EN' ? dataEN : dataGE);
+  }, [language]);
 
   function renderQuestions() {
     return data.results.map((result, index) => (
-      <Question key={index} result={result} index={index} />
+      <Question key={index} result={result} index={index} language={language} handleSubmit={()=>handleSubmit()} />
     ));
   }
 
@@ -39,10 +87,21 @@ export default function App() {
     return scores;
   }
 
+  // Function to find the first unanswered question
+  function findFirstUnansweredQuestion() {
+    return chosenAnswers.findIndex(answer => answer === undefined);
+  }
+
+  // Define handleSubmit function
   function handleSubmit() {
+    const firstUnansweredIndex = findFirstUnansweredQuestion();
+    if (firstUnansweredIndex !== -1) {
+      scrollToElem(`question-${firstUnansweredIndex}`);
+      return; // Prevent submission
+    }
+
     const scores = calculateSectionScores();
 
-    // EmailJS configuration
     emailjs.send('service_9pcguwq', 'template_hre42w5', {
       to_name: 'AIIA',
       from_name: 'AIIA',
@@ -63,48 +122,48 @@ export default function App() {
 
   return (
     <Store.Provider value={{ chosenAnswers, setChosenAnswers }}>
-      <Start />
+      <Start setLanguage={setLanguage} language={language} />
       {renderQuestions()}
-      <Finish calculateSectionScores={calculateSectionScores} handleSubmit={handleSubmit} emailSent={emailSent} />
+      <Finish calculateSectionScores={calculateSectionScores} handleSubmit={handleSubmit} emailSent={emailSent} language={language} />
     </Store.Provider>
   );
 }
 
-export function Question({ result, index }) {
+export function Question({ result, index, language,handleSubmit }) {
   return (
     <section id={`question-${index}`} className="fullpage-center">
       <h3>
         {index + 1}. {renderHTML(result.question)}
       </h3>
       <div className="answers">
-        <Answers result={result} parentIndex={index} />
+        <Answers result={result} parentIndex={index} language={language} />
       </div>
       <section className="btn-group" style={{ display: "flex" }}>
         {index !== 0 && (
           <Button
-            text="prev"
+            text={translations[language].prev}
             func={() => scrollToElem(`question-${index - 1}`)}
           />
         )}
         {index !== totalQuestions - 1 && (
           <Button
-            text="next"
+            text={translations[language].next}
             func={() => scrollToElem(`question-${index + 1}`)}
           />
         )}
         {index === totalQuestions - 1 && (
-          <Button text="finish" func={() => scrollToElem("finish")} />
+          <Button text={translations[language].finish} func={() => handleSubmit()} />
         )}
       </section>
     </section>
   );
 }
 
-export function Answers({ result, parentIndex }) {
+export function Answers({ result, parentIndex, language }) {
   const answerOptions = [
-    "Never applies to me",
-    "Sometimes applies to me",
-    "Often applies to me"
+    translations[language].never,
+    translations[language].sometimes,
+    translations[language].often
   ];
 
   return answerOptions.map((answer, index) => (
@@ -145,35 +204,39 @@ function Button({ text, func }) {
   );
 }
 
-function Start() {
+function Start({ setLanguage, language }) {
   return (
     <section className="fullpage-center" id="start">
-      <h1>Learning Style Assessment</h1>
-      <h2>How do these statements apply to you?</h2>
+      <h1>{translations[language].assessmentTitle}</h1>
+      <h2>{translations[language].assessmentSubtitle}</h2>
       <Button text="Let's go!" func={() => scrollToElem("question-0")} />
+      <div className="language-switch">
+        <button onClick={() => setLanguage('EN')} disabled={language === 'EN'}>English</button>
+        <button onClick={() => setLanguage('GE')} disabled={language === 'GE'}>ქართულად</button>
+      </div>
     </section>
   );
 }
 
-function Finish({ calculateSectionScores, handleSubmit, emailSent }) {
+function Finish({ calculateSectionScores, handleSubmit, emailSent, language }) {
   const { chosenAnswers } = useContext(Store);
   const scores = calculateSectionScores();
   const textCompleted = (
     <Fragment>
-      <h3>Well done!</h3>
-      <h4>Scores for each section:</h4>
-      <p>Section One (Visual): {scores.SECTION_ONE}</p>
-      <p>Section Two (Auditory): {scores.SECTION_TWO}</p>
-      <p>Section Three (Kinesthetic): {scores.SECTION_THREE}</p>
-      <Button text="Submit Scores" func={handleSubmit} />
-      {emailSent && <p>Email sent successfully!</p>}
+      <h3>{translations[language].wellDone}</h3>
+      <h4>{translations[language].sectionScores}</h4>
+      <p>{translations[language].sectionOne}: {scores.SECTION_ONE}</p>
+      <p>{translations[language].sectionTwo}: {scores.SECTION_TWO}</p>
+      <p>{translations[language].sectionThree}: {scores.SECTION_THREE}</p>
+      <Button text={translations[language].submit} func={handleSubmit} />
+      {emailSent && <p>{translations[language].emailSuccess}</p>}
     </Fragment>
   );
 
   const textIncomplete = (
     <Fragment>
-      <h4>Oops, looks like you haven't answered all the questions</h4>
-      <p>Scroll up to see which questions you've missed out on.</p>
+      <h4>{translations[language].incompleteMessage}</h4>
+      <p>{translations[language].scrollMessage}</p>
     </Fragment>
   );
 
